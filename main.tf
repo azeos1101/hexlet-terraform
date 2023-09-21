c1897ab24b5267cf75b14af02c2267e4a638b54c
Dmitriy Pavlenko <azeos1101@ya.ru>
2023-09-21 17:44:12 +0300

    Create full project

--- main.tf
+++ main.tf
@@ -17,16 +17,61 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_droplet" "web1" {
+resource "digitalocean_droplet" "tf-web-1" {
   image  = "ubuntu-22-04-x64"
   name   = "tf-web-1"
   region = "ams3"
   size   = "s-1vcpu-1gb"
 }
 
-resource "digitalocean_droplet" "web2" {
+resource "digitalocean_droplet" "tf-web-2" {
   image  = "ubuntu-22-04-x64"
   name   = "tf-web-2"
   region = "ams3"
   size   = "s-1vcpu-1gb"
+}
+
+resource "digitalocean_database_cluster" "tf-db-1" {
+  name       = "tf-db-1"
+  engine     = "pg"
+  version    = "15"
+  size       = "db-s-1vcpu-1gb"
+  region     = "ams3"
+  node_count = 1
+}
+
+resource "digitalocean_loadbalancer" "loadbalancer" {
+  name   = "lb-1"
+  region = "ams3"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 8080
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+  }
+
+  droplet_ids = [
+    digitalocean_droplet.tf-web-1.id,
+    digitalocean_droplet.tf-web-2.id,
+  ]
+}
+
+resource "digitalocean_project" "tf-playground" {
+  name        = "tf-playground"
+  description = "A project to represent development resources."
+  purpose     = "Web Application"
+  environment = "Development"
+  resources   = [
+    digitalocean_droplet.tf-web-1.urn,
+    digitalocean_droplet.tf-web-2.urn,
+    digitalocean_database_cluster.tf-db-1.urn,
+    digitalocean_loadbalancer.loadbalancer.urn
+  ]
 }
